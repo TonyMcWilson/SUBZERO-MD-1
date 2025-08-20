dadaae48063c35a7e2692e83192ac74578157bac
TonyMcWilson <114802851+TonyMcWilson@users.noreply.github.com>
2025-08-20 11:01:02 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;eUtzxaCb#QtLucGFvT7998Cmi02EWLBG0RCfHPJciQ3Vj4zKSMLo", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "256708252582", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
